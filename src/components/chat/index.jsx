b984172d3f4cf7bd7c37d2c49ba6bfba43506c9a
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-01 19:49:00 +0900

    chat

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
+import CustomHeader from "../customHeader";
+import StandardMessageForm from "../customMessageForm/StandardMessageForm";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "testuser",
+    "1234"
+  );
+  return (
+    <div style={{ flexBasis: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat) => <CustomHeader chat={chat} />}
+        renderMessageForm={(props) => {
+          return (
+            <StandardMessageForm props={props} activeChat={chatProps.chat} />
+          );
+        }}
+      />
+    </div>
+  );
 };
 
 export default Chat;
